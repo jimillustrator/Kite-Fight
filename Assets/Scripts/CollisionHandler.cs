2f1c9b1329fa120977cb373d0c070ad95362e675
Jim McNeill <jimillustrator@gmail.com>
2021-07-10 10:54:45 -0700

    Added my own array to the Collision Handler script to selectively turn off fighter parts on explosion

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
+    [SerializeField] GameObject[] fighterParts;
 
     void OnTriggerEnter(Collider other)
     {
@@ -15,6 +17,8 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence()
     {
+        TurnOffParts();
+        crashVFX.Play();
         GetComponent<PlayerController>().enabled = false;
         Invoke("ReloadLevel", loadDelay);
     }
@@ -23,5 +27,14 @@ public class CollisionHandler : MonoBehaviour
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void TurnOffParts()
+    {
+        int i = 0;
+        for (i = 0; i < fighterParts.Length; ++i)
+        {
+            fighterParts[i].GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 }
