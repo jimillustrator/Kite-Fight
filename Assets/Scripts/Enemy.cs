25bfb81aaa1c40b9212d32692714086051cb0e1c
Jim McNeill <jimillustrator@gmail.com>
2023-06-27 15:41:22 -0700

    Added Enemy Explosion SFX and updated Enemy Script (also added Recorder Asset from the Unity Registry to Unity itself)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject deathFX;
     [SerializeField] GameObject hitVFX;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 1; //number of times the enemy needs to be hit
@@ -37,16 +37,16 @@ public class Enemy : MonoBehaviour
 
     void ProcessHit()
     {
-        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parentGameObject.transform;
+        GameObject fx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parentGameObject.transform;
         hitPoints--;
     }
 
     void KillEnemy()
     {
         scoreBoard.IncreaseScore(scorePerHit);
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parentGameObject.transform;
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 }
